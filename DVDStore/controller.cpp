655f0b4f10bc5f4d409652dc32a384340152f779
cheese-sauce <jack@chappel.com>
2015-05-24 21:51:47 +1000

    author and add existing

--- DVDStore/controller.cpp
+++ DVDStore/controller.cpp
@@ -47,6 +47,8 @@ void Controller::printHeader() {
             "#     #  #   #  #     #    #   #   #       #   # #    #    ####### #       \n"
             "#     #   # #   #     #    #    #  #       #    ##    #    #     # #       \n"
             "######     #    ######     #     # ####### #     #    #    #     # ####### \n";
+
+    cout << "Authors: \nKevin Upton n9214305 \nJack Chappell n9185216";
 }
 
 void Controller::attemptAuth() {
@@ -357,7 +359,7 @@ string &Controller::trim(std::string &s) {
 }
 
 void Controller::createHeaderContent(string title) {
-    cout << string(13,'\n');
+    cout << string(10,'\n');
     cout << string(45, '#') << endl;
     cout << "\t" + title + "\t\n";
     cout << string(45, '#') << endl;
@@ -424,6 +426,8 @@ void Controller::performAddDVDNew() {
 void Controller::performAddDVDExisting() {
     cout << string(15,'\n');
     createHeaderContent("Add DVD of an Existing Movie");
+    cout << "Please enter movie title: ";
+    string title = getTextInput();
     try {
         //app.registerMovieFromExisting()
     } catch (exception e) {
